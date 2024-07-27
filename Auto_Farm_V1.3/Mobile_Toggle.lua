2833a72e0ba3fc2659ded6b41c06806b83a0973d
EpicPug <112693990+EpicPug@users.noreply.github.com>
2024-07-26 21:24:54 -0700

    Create Mobile_Toggle.lua

---
+++ Auto_Farm_V1.3/Mobile_Toggle.lua
@@ -0,0 +1,42 @@
+-- toggle button for mobile users.
+
+if game.CoreGui:FindFirstChild("Auto Farm V1.3 Toggle") then
+	return
+end
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "Auto Farm V1.3 Toggle"
+ScreenGui.Parent = game.CoreGui
+ScreenGui.IgnoreGuiInset = true
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+local Frame = Instance.new("Frame")
+Frame.Parent = ScreenGui
+Frame.BackgroundTransparency = 1
+Frame.Position = UDim2.new(0.014154871, 0, 0.493710697, 0)
+Frame.Size = UDim2.new(0.016652789, 0, 0.0786163509, 0)
+
+local TextButton = Instance.new("TextButton")
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+TextButton.Position = UDim2.new(0, 0, 0.230000004, 0)
+TextButton.Size = UDim2.new(0, 92, 0, 27)
+TextButton.Font = Enum.Font.FredokaOne
+TextButton.Text = "Close"
+TextButton.TextColor3 = Color3.fromRGB(139, 139, 139)
+TextButton.TextSize = 14
+TextButton.MouseButton1Click:Connect(function()
+	local Gui = game.CoreGui:FindFirstChild("Auto Farm V1.3")
+	if Gui then
+		Gui.Enabled = not Gui.Enabled
+		TextButton.Text = not Gui.Enabled and "Open" or "Close"
+	end
+end)
+
+local UICorner = Instance.new("UICorner")
+UICorner.Parent = TextButton
+
+local UIStroke = Instance.new("UIStroke")
+UIStroke.Parent = TextButton
+UIStroke.Color = Color3.fromRGB(39, 39, 39)
+UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
